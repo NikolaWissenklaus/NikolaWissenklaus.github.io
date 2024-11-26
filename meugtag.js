d470172f4b15eb79530a7e316c0c1290d8af7256
Francisco Lucas <62573687+NikolaWissenklaus@users.noreply.github.com>
2024-11-26 09:06:53 -0300

    Update meugtag.js

--- meugtag.js
+++ meugtag.js
@@ -14366,6 +14366,7 @@
                 f ? f = "" + f : (f = hp(), g = 7, a.metadata.is_first_visit = a.metadata.is_new_to_site = !0);
                 gG(a, f, g)
             }
+            console.log("bbbbbbb");
             var n = Math.floor(a.metadata.event_start_timestamp_ms /
                     1E3),
                 p = void 0;
@@ -14378,9 +14379,9 @@
             a.metadata.is_first_visit = !1;
             a.metadata.is_session_start = !1;
             a.metadata.join_timer_sec = 0;
-            console.log("et >>>", u.Xh);
-            
+            console.log("ccccccccccccc");
             u && u.Xh && (a.metadata.join_timer_sec = Math.max(0, u.Xh - Math.max(0, n - u.cf)));
+            console.log("dddddddddd");
             var v = !1;
             u || (v = a.metadata.is_first_visit = !0, u = {
                 sessionId: String(n),
@@ -14390,6 +14391,7 @@
                 Mc: !1,
                 Dd: void 0
             });
+            console.log("eeeeeeeeee");
             n > u.cf + q * 60 && (v = !0, u.sessionId = String(n), u.Qc++, u.Nd = !1, u.Dd = void 0);
             if (v) a.metadata.is_session_start = !0, d.qm(a);
             else if (d.gm() > r || a.eventName === O.g.ac) u.Nd = !0;
